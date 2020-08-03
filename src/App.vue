90972e17e6ee227dbe30b4018fa55b456bf92c70
Andrey Ospennikov <ospas312@ya.ru>
2020-08-04 01:11:39 +0300

    first commit create project

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to My Vue.js App Photo_Edit_Uploader"/>
+    <DrugDrop />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import DrugDrop from './components/DrugDrop.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    DrugDrop
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
